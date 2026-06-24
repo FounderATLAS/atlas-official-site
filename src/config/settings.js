7ee61e9062b456eef4600a7db809d17baf066f51
FounderATLAS <founder@atla-solutions.com>
2026-06-24 19:06:45 -0400

    add a new theme file and update the theme selector

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
 	url: `https://www.atla-solutions.com/`, // No trailing slash!
 	name: `ATLAS`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
-	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+	enableThemeSwitcher: false,
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
